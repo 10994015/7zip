0cb93dc7a613bd923f9a76428279eb4dada3c5ac
10994015 <a0938599191@gmail.com>
2021-12-19 00:05:09 +0800

    1219

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-var OpeningAnimation = document.getElementsByClassName('OpeningAnimation')[0];
-    setTimeout(()=>{
-        OpeningAnimation.style.opacity="0";
-    },11000)
-    setTimeout(()=>{
-        OpeningAnimation.style.display="none";
-    },12000);
+// var OpeningAnimation = document.getElementsByClassName('OpeningAnimation')[0];
+//     setTimeout(()=>{
+//         OpeningAnimation.style.opacity="0";
+//     },11000)
+//     setTimeout(()=>{
+//         OpeningAnimation.style.display="none";
+//     },12000);
